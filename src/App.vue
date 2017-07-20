05c7ef26df39af88f7f24f0bc66bfb2c4c643d83
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-20 14:29:26 -0400

    vue project setup

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app light>
+    <v-toolbar>
+      <v-toolbar-title>
+        What to eat tonight
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat>
+          <v-icon left>restaurant</v-icon>
+          View Menu
+        </v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+    <main>
+
+    </main>
+  </v-app>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style lang="stylus">
+  @import './stylus/main'
+</style>
