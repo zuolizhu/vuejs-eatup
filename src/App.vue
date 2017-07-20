aa7691e9bade02553405e491b23018efac48f88e
zuolizhu <zuolizhu5960x@gmail.com>
2017-07-20 17:12:46 -0400

    update vuetify

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,6 @@
         <v-list-tile 
         v-for="item in menuItems" 
         :key="item.title" 
-        router 
         :to="item.link">
           <v-list-tile-action>
             <v-icon>{{ item.icon }}</v-icon>
@@ -19,7 +18,7 @@
 
     <v-toolbar class="primary">
       <v-toolbar-side-icon 
-      @click.native.stop="sideNav =! sideNav" 
+      @click.stop="sideNav =! sideNav" 
       class="hidden-sm-and-up"></v-toolbar-side-icon>
       <v-toolbar-title>
         <router-link to="/" tag="span" style="cursor: pointer">What to eat tonight</router-link>  
@@ -32,7 +31,6 @@
         flat 
         v-for="item in menuItems" 
         :key="item.title" 
-        router 
         :to="item.link">
           <v-icon left>{{ item.icon }}</v-icon>
           {{ item.title }}
