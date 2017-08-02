40a49c64a42f1fd08d2208082a237dca53f6f48f
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-02 17:15:45 -0400

    text input

--- src/components/Eatup/CreateEatup.vue
+++ src/components/Eatup/CreateEatup.vue
@@ -1,5 +1,85 @@
 <template>
-  <div>
-      <p>The Create Eatup Page</p>
-  </div>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <h4 class="primary--text">Create a new Eatup</h4>
+      </v-flex>
+    </v-layout>
+    <v-layout row>
+      <v-flex xs12>
+        <form>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field 
+              name="title"
+              label="Title"
+              id="title"
+              v-model="title"
+              required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field 
+              name="location"
+              label="Location"
+              id="location"
+              v-model="location"
+              required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field 
+              name="imageURL"
+              label="Image URL"
+              id="imageURL"
+              v-model="imageURL"
+              required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <img :src="imageURL">
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-text-field 
+              name="description"
+              label="Description"
+              id="description"
+              v-model="description"
+              multi-line
+              required>
+              </v-text-field>
+            </v-flex>
+          </v-layout>
+          <v-layout row>
+            <v-flex xs12 sm6 offset-sm3>
+              <v-btn class="primary">
+                Create Eatup
+              </v-btn>
+            </v-flex>
+          </v-layout>
+        </form>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: '',
+      location: '',
+      imageURL: '',
+      description: ''
+    }
+  }
+}
+</script>
