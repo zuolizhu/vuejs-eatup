ab48c38ee6149a9e406bf338d7971e5ca66e92c1
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-02 17:20:40 -0400

    resize image

--- src/components/Eatup/CreateEatup.vue
+++ src/components/Eatup/CreateEatup.vue
@@ -43,7 +43,7 @@
           </v-layout>
           <v-layout row>
             <v-flex xs12 sm6 offset-sm3>
-              <img :src="imageURL">
+              <img :src="imageURL" height="175">
             </v-flex>
           </v-layout>
           <v-layout row>
