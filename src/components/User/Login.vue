a684edcab69dafeefe3180c1fbc13cd1924dd039
zuolizhu <zuolizhu5960x@gmail.com>
2017-08-21 16:12:10 -0400

    Auth state feedback with Vuex adding to login page

--- src/components/User/Login.vue
+++ src/components/User/Login.vue
@@ -1,5 +1,10 @@
 <template>
   <v-container>
+    <v-layout row v-if="error">
+      <v-flex xs12 sm6 offset-sm3>
+        <app-alert @dismissed="onDismissed" :text="error.message"></app-alert>
+      </v-flex>
+    </v-layout>
     <v-layout row>
       <v-flex xs12 sm6 offset-sm3>
         <v-card-text>
@@ -29,7 +34,12 @@
               </v-layout>
               <v-layout row>
                 <v-flex xs12>
-                  <v-btn type="submit">Login</v-btn>
+                  <v-btn type="submit" :disable="loading" :loading="loading">
+                    Login
+                    <span slot="loader" class="custom-loader">
+                      <v-icon light>cached</v-icon>
+                    </span>
+                  </v-btn>
                 </v-flex>
               </v-layout>
             </form>
@@ -52,6 +62,12 @@ export default {
   computed: {
     user () {
       return this.$store.getters.user
+    },
+    error () {
+      return this.$store.getters.error
+    },
+    loading () {
+      return this.$store.getters.loading
     }
   },
   watch: {
@@ -64,6 +80,9 @@ export default {
   methods: {
     onLogin () {
       this.$store.dispatch('loginUser', {email: this.email, password: this.password})
+    },
+    onDismissed () {
+      this.$store.dispatch('clearError')
     }
   }
 }
--- src/components/User/Signup.vue
+++ src/components/User/Signup.vue
@@ -98,7 +98,6 @@ export default {
       this.$store.dispatch('signUserUp', {email: this.email, password: this.password})
     },
     onDismissed () {
-      console.log('Dismissed Alert!')
       this.$store.dispatch('clearError')
     }
   }
