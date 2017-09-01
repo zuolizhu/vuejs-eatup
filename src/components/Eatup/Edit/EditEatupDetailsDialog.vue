aca3cdb3c4ebca21d793409a5b93bdcc4e97fea2
zuolizhu <zuolizhu5960x@gmail.com>
2017-09-01 13:10:58 -0400

    dialog which enable user to edit posted event

---
+++ src/components/Eatup/Edit/EditEatupDetailsDialog.vue
@@ -0,0 +1,7 @@
+<template>
+    <v-dialog width = "360px" persistent>
+        <v-btn floating accent slot="activator">
+            <v-icon>edit</v-icon>
+        </v-btn>
+    </v-dialog>
+</template>
