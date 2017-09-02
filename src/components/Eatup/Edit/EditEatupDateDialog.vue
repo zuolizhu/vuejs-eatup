6b8b27b99ee1c55a2b992c6b7c2899f038a599d6
zuolizhu <zuolizhu5960x@gmail.com>
2017-09-02 12:59:16 -0400

    date modifiable component added

---
+++ src/components/Eatup/Edit/EditEatupDateDialog.vue
@@ -0,0 +1,55 @@
+<template>
+    <v-dialog width = "300px" persistent v-model="editDialog">
+        <v-btn accent slot="activator">
+            Change Date
+        </v-btn>
+        <v-card>
+            <v-container>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-card-title>Edit Eatup Date</v-card-title>
+                    </v-flex>
+                </v-layout>
+                <v-divider></v-divider>
+                <v-layout row wrap>
+                    <v-flex xs12>
+                        <v-date-picker 
+                        v-model="editableDate" 
+                        style="width: 100%" actions>
+                            <template scope="{save, cancel}">
+                                <v-btn 
+                                    class="blue--text darken-1" 
+                                    flat 
+                                    @click.native="editDialog = false"></v-btn>
+                                <v-btn 
+                                    class="blue--text darken-1" 
+                                    flat 
+                                    @click.native="onSaveChanges = false"></v-btn>
+                            </template>
+                        </v-date-picker>
+                    </v-flex>
+                </v-layout>
+            </v-container>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+  props: ['eatup'],
+  data () {
+      return {
+          editDialog: false,
+          editableDate: null
+      }
+  },
+  methods: {
+      onSaveChanges () {
+          
+      }
+  },
+  created () {
+      this.editableDate = new Date(this.eatup.date)
+  }
+}
+</script>
